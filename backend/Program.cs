e3d89b8a10924517105d6c584832ee5a61ecac21
Victor Paiva <victor_eduardof@hotmail.com>
2026-07-16 22:03:20 -0300

    feat(Program.cs): Add the configuration of the connection with DbContext. Add an endpoint for test mannualy with swagger

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,9 +1,16 @@
+using backend.Data;
+using backend.Routes;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configuring AppDbContexto to use SQLite and the DB
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source= HomeWallet.sqlite"));
 
 var app = builder.Build();
 
@@ -14,7 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// code here
+// Routes for Users and Transaction
+UsersRoute.UsersRoutes(app);
 
 
 app.UseHttpsRedirection();
