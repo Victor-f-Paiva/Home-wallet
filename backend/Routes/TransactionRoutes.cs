82b677696a2ec52b124b1d06b986da254687ab34
Victor Paiva <victor_eduardof@hotmail.com>
2026-07-18 12:46:31 -0300

    feat(endpoints): add routes por create, read and delete user and transactions

--- backend/Routes/TransactionRoutes.cs
+++ backend/Routes/TransactionRoutes.cs
@@ -1,12 +1,77 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using backend.Data;
+using backend.DTOs;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Routes
 {
     public static class TransactionRoutes
     {
-        
+        public static void TransactionRoute(WebApplication app)
+        {
+            // creating a fixed route
+            var route = app.MapGroup("Transaction");
+
+            //POST
+            route.MapPost("", 
+            async (TransactionRequest req, AppDbContext context) =>
+            {
+                //validating if userIs exists
+                var user = await context.UsersTable.FindAsync(req.userId);
+                if (user == null)
+                {
+                    return Results.NotFound($"User with id {req.userId} Not Found");
+                }
+
+                //creating and saving the transaction
+                var transaction = new Transaction(req.description, req.value, req.type, req.userId);
+                await context.TransactionsTable.AddAsync(transaction);
+                await context.SaveChangesAsync();
+
+                // seting a response
+                var response = new TransactionResponse(transaction.Id, transaction.Description, transaction.Value, transaction.Type.ToString(), transaction.UserId);
+                return Results.Created($"Transaction created for user id {req.userId}", response);
+            });
+
+            //GET
+            route.MapGet("",
+            async (AppDbContext context) =>
+            {
+                var transactionsList = await context.TransactionsTable.ToListAsync();
+                return Results.Ok(transactionsList);
+            });
+
+            //GET BY ID
+            route.MapGet("/{id}",
+            async (int id, AppDbContext context) =>
+            {
+                var transaction = await context.TransactionsTable.FindAsync(id);
+
+                //verifying if there is a transaction
+                if (transaction == null)
+                {
+                    return Results.NotFound($"Transactio id {id} Not Found");
+                }
+                return Results.Ok(transaction);
+            });
+
+            //DELETE
+            route.MapDelete("/{id}",
+            async (int id, AppDbContext context) =>
+            {
+                var transaction = await context.TransactionsTable.FindAsync(id);
+
+                //verifying if there is the transaction
+                if (transaction == null)
+                {
+                    return Results.NotFound($"Transaction id {id} Not Found");
+                }
+
+                //removing and saving
+                context.TransactionsTable.Remove(transaction);
+                await context.SaveChangesAsync();
+                return Results.Ok($"Transaction id {id} Deleted");
+            });
+        }
     }
 }
--- backend/Routes/UsersRoute.cs
+++ backend/Routes/UsersRoute.cs
@@ -1,4 +1,8 @@
+using backend.Data;
+using backend.DTOs;
 using backend.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Routes
 {
@@ -9,7 +13,53 @@ namespace backend.Routes
     {
         public static void UsersRoutes(WebApplication app)
         {
-            app.MapGet(pattern:"User", () => new Users("Victor", 31));
+            // making a unchenginble route
+            var route = app.MapGroup("Users");
+
+            //POST
+            route.MapPost("", 
+            async (UsersRequest req, AppDbContext context) =>
+            {
+                //create an object, add to the schemma, saving it and return a message
+                var user = new Users(req.name, req.age);
+                await context.AddAsync(user);
+                await context.SaveChangesAsync();
+                return Results.Created("User family created", user);
+            });
+
+            //GET
+            route.MapGet("", 
+            async (AppDbContext context) =>
+            {
+                var users_list = await context.UsersTable.ToListAsync();
+                return Results.Ok(users_list);
+            });
+
+            //GET BY ID
+            route.MapGet("/{id}", 
+            async (int id, AppDbContext context) =>
+            {
+                var user = await context.UsersTable.FindAsync(id);
+                if (user == null)
+                {
+                    return Results.NotFound($"User id {id} Not Found");
+                }
+                return Results.Ok(user);
+            });
+
+            //DELETE
+            route.MapDelete("/{id}", 
+            async (int id, AppDbContext context) =>
+            {
+                var user = await context.UsersTable.FindAsync(id);
+                if (user == null)
+                {
+                    return Results.NotFound("Family user not found");
+                }
+                context.UsersTable.Remove(user);
+                await context.SaveChangesAsync();
+                return Results.Ok($"Family user, id {id}, removed");
+            });
         }
     }
 }
