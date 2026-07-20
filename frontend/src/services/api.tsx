a2863c2fd2a2667e8d42ba47f1ca7a3ffc62ac7a
Victor Paiva <victor_eduardof@hotmail.com>
2026-07-20 15:26:57 -0300

    feat(services): implement services for users, transaction and report

---
+++ frontend/src/services/api.tsx
@@ -0,0 +1,39 @@
+import type {UserRequest, TransactionRequest, FinancialReportResponse } from '../types/index';
+
+const BASE_URL = 'http://localhost:5233';
+
+export const apiService = {
+  async createUser(data: UserRequest): Promise<any> {
+    const response = await fetch(`${BASE_URL}/Users`, { 
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) throw new Error('Error to create a new User');
+    return response.json();
+  },
+
+  async deleteUser(id: number): Promise<any> {
+    const response = await fetch(`${BASE_URL}/Users/${id}`, {
+      method: 'DELETE',
+    });
+    if (!response.ok) throw new Error('Error to delete an user');
+    return response.status === 204 ? null : response.json();
+  },
+
+  async createTransaction(data: TransactionRequest): Promise<any> {
+    const response = await fetch(`${BASE_URL}/Transaction`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) throw new Error('Error to create a new transaction');
+    return response.json();
+  },
+
+  async getFinancialReport(): Promise<FinancialReportResponse> {
+    const response = await fetch(`${BASE_URL}/Users/HouseExpenses`);
+    if (!response.ok) throw new Error('Error to find the report');
+    return response.json();
+  }
+};
