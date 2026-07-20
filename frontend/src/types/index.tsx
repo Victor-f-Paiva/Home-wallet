274c75bd20a9fa018f3f3f1ae06e120212719d83
Victor Paiva <victor_eduardof@hotmail.com>
2026-07-20 15:21:20 -0300

    feat(types): add interfaces to request and response for user and transaction

---
+++ frontend/src/types/index.tsx
@@ -0,0 +1,40 @@
+//Map the enum to be equals to the backend
+export enum TransactionType {
+    Income = 0,
+    Expense = 1     
+}
+
+
+// Set interface for a transaction request
+export interface TransactionRequest {
+    description: string;
+    value: number;
+    type: TransactionType;
+    userId: number;
+}
+
+
+//Set interface for a user request
+export interface UserRequest{
+    name: string;
+    age: number;
+}
+
+
+// Set response for one user with his transactions
+export interface UserReport{
+    id: number,
+    name: string;
+    income: number;
+    expenses: number;
+    netBalance: number;
+}
+
+
+// Set the response for the Fiancial report of the house
+export interface FinancialReportResponse{
+    usersList: UserReport[];
+    totalIncome: number;
+    totalExpenses: number;
+    totalNetBalance: number;
+}
